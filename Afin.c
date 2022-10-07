4dba1010aa9b9509fc542b0dd5818a5535780357
FJArribas <francisco.arribas01@estudiante.uam.es>
2022-10-07 05:17:27 +0200

    Main

--- Afin.c
+++ Afin.c
@@ -4,8 +4,8 @@
 #include <gmp.h>
 
 
-void euclides (mpz_t a, mpz_t b, mpz_t q){
-
+void euclides (mpz_t a, mpz_t b, mpz_t q)
+{
     mpz_t r0, r1;
 
     mpz_init(r0);
@@ -37,12 +37,11 @@ void euclides (mpz_t a, mpz_t b, mpz_t q){
 
     mpz_clear(r0);
     mpz_clear(r1);
-
 }
 
 
-void euclides_extended (mpz_t a, mpz_t b, mpz_t u0, mpz_t v0){
-
+void euclides_extended (mpz_t a, mpz_t b, mpz_t u0, mpz_t v0)
+{
     mpz_t u1, v1, u2, v2, aux, r, r0, r1, q;
 
     /* Initializing values */
@@ -105,30 +104,92 @@ void euclides_extended (mpz_t a, mpz_t b, mpz_t u0, mpz_t v0){
 
 int main (int argc, char *argv[])
 {
-    int cifra, tam, i = 1;
+    int tam, i = 1;
     unsigned long int h = 100;
-    char* str_a, str_b;
+    char *str_a, *str_b;
     FILE *entrada, *salida;
     mpz_t a, b, q, u, v;
 
 
-    if(argc <= 1 || argc > 12) // TODO: Comprobar que este es el número máximo de args
-        printf("\nPrograma AFÍN ejecutado. Uso del programa:"
-               "afin {-C | -D} {-m |Zm|} {-a N×} {-b N+} [-i filein] [-o fileout]"
-               "-C: El programa cifra"
-               "-D: El programa descifra"
-               "-m: Tamaño del espacio de texto cifrado"
-               "-a: Coeficiente multiplicativo de la función afín"
-               "-b: Término constante de la función afín"
+    if(argc <= 8 || argc > 12)
+    {
+        printf("\nPrograma AFÍN ejecutado. Uso del programa:\n"
+               "afin {-C | -D} {-m |Zm|} {-a N×} {-b N+} [-i filein] [-o fileout]\n"
+               "-C: El programa cifra\n"
+               "-D: El programa descifra\n"
+               "-m: Tamaño del espacio de texto cifrado\n"
+               "-a: Coeficiente multiplicativo de la función afín\n"
+               "-b: Término constante de la función afín\n"
                "-i: Fichero de entrada\n"
                "-o: Fichero de salida\n");
-
-    if(argc >= 2)
+    }
+    else
     {
-        if (strcmp(argv[2], "-C") == 0)
-            cifra = 1;  // Cifra
+        if (strcmp(argv[1], "-C") == 0 || strcmp(argv[1], "-c") == 0) // Cifra
+        {
+            if ((argc > 8) && ((strcmp(argv[8], "-i") == 0 || strcmp(argv[8], "-I") == 0))) // Entrada por fichero
+            {
+                entrada = fopen (argv[9], "r");
+                if (entrada == NULL)
+                {
+                    printf ("Error al abrir el fichero de entrada\n");
+                    return -1;
+                }
+                
+                fseek (entrada, 0, SEEK_END);
+                tam = ftell (entrada);
+                rewind (entrada);
+                str_a = malloc ((tam + 1) * sizeof (char));
+                fread (str_a, 1, tam, entrada);
+                fclose (entrada);
+            }
+            else    // Entrada por teclado
+            {
+                printf ("\nIntroducir entrada: ");
+                scanf ("%m[^\n]%*c", &str_a);
+                tam = strlen (str_a);
+            }
+            
+            // TODO: CIFRAR
+            // c = cifrar (a, b, msg, str_a)
+            
+            // Scanf -> Fichero
+            if ((argc < 11 ) && ((strcmp(argv[8], "-o") == 0 || strcmp(argv[8], "-O") == 0)))
+            {
+                salida = fopen (argv[9], "w");
+                // TODO fputs (c, salida);
+                fclose (salida);
+            }
+            // Fichero -> Printf
+            else if ((argc < 11 ) && ((strcmp(argv[8], "-o") != 0 && strcmp(argv[8], "-O") != 0)))
+            {
+                printf ("Mensaje cifrado: %s", c);
+            }
+            // Scanf -> Printf
+            else if (argc == 8)
+            {
+                printf ("Mensaje cifrado: %s", c);
+            }
+            // Fichero -> Fichero
+            else
+            {
+                salida = fopen (argv[11], "w");
+                // TODO fputs (c, salida);
+                fclose (salida);
+            }
+            printf("\n")
+            // free (c);
+            free (str_a);
+        }
+        else if (strcmp(argv[1], "-D") == 0 || strcmp(argv[1], "-d") == 0) // Descifra
+        {
+            // TODO Lo mismo basicamente
+        }
         else
-            cifra = 0;  // Descifra
+        {
+            printf("\nError en los parámetros\n")
+        }
+    }
 
         if (strcmp(argv[3], "-m") == 0)
             tam = atoi(argv[4]); // Tamaño del espacio del texto
